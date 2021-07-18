ea6813a3d2aab3b6abc8e221b580633a920cddce
Lore Vincit <lore@dagobah.Endor>
2021-07-18 01:57:25 -0400

    js funcionando

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,11 @@
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-    $('[data-toggle="popover"]').popover()
-  })
+  $('[data-toggle="tooltip"]').tooltip()
+  $("h4").dblclick(function(){
+    $(this).css({
+        "color": "red",
+    });
+});
+$("[id=enviarCorreo]").click(function(){
+  alert("El correo fue enviado correctamente");
+});
+})
