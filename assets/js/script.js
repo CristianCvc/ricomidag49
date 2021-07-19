8db966dbb853fb9e84ed82840d4809dae2d2d332
Lore Vincit <lore@dagobah.Endor>
2021-07-19 17:17:08 -0400

    se mejora responsividad, ya no aparece barra de desplazamiento, se añade jquery toogle que me faltaba pero con detalles a arreglar y todavia no se como, arreglos pendientes

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,11 @@ $(function () {
         "color": "red",
     });
 });
-$("[id=enviarCorreo]").click(function(){
+  $("[id=enviarCorreo]").click(function(){
   alert("El correo fue enviado correctamente");
 });
+  $(".alternar-tarjeta").on("click",function(e){
+  $(this).next().toggle();
+  e.preventDefault();
+})
 })
